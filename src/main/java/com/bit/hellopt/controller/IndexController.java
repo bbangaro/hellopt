10db781c6f93153d614480579509469c4d847c1e
밀가루 <bbangaro@gmail.com>
2020-02-12 18:04:47 +0900

    2020-02-12

---
+++ src/main/java/com/bit/hellopt/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.bit.hellopt.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+	
+	@GetMapping("/")
+	public String home() {
+		logger.info("home controller get mapping");
+		return "home";
+	}
+	
+}
