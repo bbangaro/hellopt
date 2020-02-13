26da7c584b1cd751baf4d10114c2ab7842499eb2
DanHoBakMaCha <scvgo23@gmail.com>
2020-02-13 17:02:34 +0900

    인증이 필요한 페이지

--- src/main/java/com/bit/hellopt/config/SecurityConfig.java
+++ src/main/java/com/bit/hellopt/config/SecurityConfig.java
@@ -6,49 +6,40 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//auth.userDetailsService(service);	
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password(passwordEncoder().encode("password")).roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/hellopt", "/hellopt/home").permitAll()
+				.antMatchers("/", "home").permitAll()
 				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/hellopt/login")
+				
+			.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/home")
+				.failureUrl("/login?error=true")
 				.permitAll()
-				.and()
-			.logout()
-				.logoutUrl("/hellopt/logout")
-				.permitAll();
-	}
-	
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+			.and()
+				.logout()
+				.logoutSuccessUrl("/home")
+				.invalidateHttpSession(true)
+				.permitAll()
+			.and()
+				.csrf().disable();
 	}
 
 	@Bean
