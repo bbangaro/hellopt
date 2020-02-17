ad330639da0bc30a76a31e2a07bd237760d0bffe
DanHoBakMaCha <scvgo23@gmail.com>
2020-02-17 16:14:52 +0900

    add  componentScan to rootContext for spring security

--- src/main/java/com/bit/hellopt/config/RootConfig.java
+++ src/main/java/com/bit/hellopt/config/RootConfig.java
@@ -3,9 +3,11 @@ package com.bit.hellopt.config;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+@ComponentScan(basePackages = { "com.bit.hellopt" })
 @Configuration
 public class RootConfig {
 
