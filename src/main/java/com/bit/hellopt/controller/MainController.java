4fa2272ced80c9c2bf557c1c39350fea3d5b2373
밀가루 <bbangaro@gmail.com>
2020-03-27 17:14:04 +0900

    소켓알림 성공

--- src/main/java/com/bit/hellopt/controller/MainController.java
+++ src/main/java/com/bit/hellopt/controller/MainController.java
@@ -26,7 +26,7 @@ public class MainController {
 	//static을 선언하면 컴파일 할 때 맨 위로 올라가서 생성 됨 
 	public static MeetingService mService;
 	
-	// 이 객체가 생성 될 때 여기에 service를 넣을거임 ( 서버 실행 > initialize 실행 > mService 대기 > service 실행 > mService 실행 )
+	// 이 객체가 생성 될 때 여기에 service를 넣을거임 ( 서버 실행 > initialize 실행(생성자들보다 먼저 실행됨) > mService 대기 > service 실행 > mService 실행 )
 	@PostConstruct
 	private void initialize() {
 		this.mService = service;
